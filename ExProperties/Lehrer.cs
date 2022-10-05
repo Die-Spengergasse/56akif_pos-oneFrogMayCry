e731c00a7e3242f19e666c630cceb2799fad6071
Io <iolena1999@gmx.at>
2022-10-05 09:43:52 +0200

    Uebung 2

---
+++ ExProperties/Lehrer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ExProperties.App
+{
+    public class Lehrer
+    {
+        public string Vorname { get; set; } = string.Empty;
+        public string? Zuname { get; set; }
+
+        public decimal? Bruttogehalt 
+        { 
+            get
+            {
+                return _bruttogehalt;
+            }
+            set
+            {
+                _bruttogehalt ??= value;
+            }
+        }
+        private decimal? _bruttogehalt;
+
+        public string? Kuerzel
+        {
+            get
+            {
+                return Zuname?.Substring(0, 3).ToUpper() ?? string.Empty;
+                //return Zuname?[..3].ToUpper() ?? string.Empty; //why?
+            }
+        }
+
+        public decimal Nettogehalt
+        {
+            get
+            {
+                return Bruttogehalt * 0.8M ?? 0M;
+            }
+        }
+    }
+}
---
+++ ExProperties/Rechteck.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ExProperties.App
+{
+    public class Rechteck
+    {
+        public int Laenge
+        {
+            get
+            {
+                return laenge;
+            }
+            set
+            {
+                laenge = (value >= 0 ? value : throw new ArgumentException("Ungültige Länge!"));
+            }
+        }
+        private int laenge;
+
+        public int Breite
+        {
+            get
+            {
+                return breite;
+            }
+            set
+            {
+                breite = (value >= 0 ? value : throw new ArgumentException("Ungültige Länge!"));
+            }
+        }
+        private int breite;
+
+        public int Flaeche
+        {
+            get
+            {
+                return Laenge * Breite;
+            }
+        }
+    }
+}
